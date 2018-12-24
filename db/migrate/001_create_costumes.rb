455b4e1ff618e7fe463f309cd8a39c0320b7c948
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-24 08:00:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,11 +11,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :costumes do |c|
-      c.string :name
-      c.integer :price
-      c.string :size
-      c.string :image_url
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
   end
 
 end
