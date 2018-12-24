ba6c70eb3cbd6e1b0708a80d422919614a17dba5
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-24 08:02:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
-# Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costumes_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.timestamps
+  end
+
+end
